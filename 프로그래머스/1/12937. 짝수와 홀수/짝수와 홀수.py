8e80dc2683680721700784a30591c16eae9707c0
Hannah <hansiyun0629@gmail.com>
2026-01-16 22:11:43 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.00 ms, Memory: 9.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.py
@@ -0,0 +1,9 @@
+def solution(num):
+    return "Even" if num % 2 == 0 else "Odd"
+    # if num == 0:
+    #     answer = "Even"
+    # if num % 2 == 0:
+    #     answer = "Even"
+    # else:
+    #     answer = "Odd"
+    # return answer
